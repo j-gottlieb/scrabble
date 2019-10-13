f20c7de64916682c48f8d06759623a49f7503d0b
jessegottlieb <jesse.m.gottlieb@gmail.com>
2019-10-13 17:52:38 -0400

    made a whoopsie with client dir name change

--- new_client/src/service/game.js
+++
@@ -1,6 +0,0 @@
-import socketIOClient from "socket.io-client";
-
-export const getNewGame = playerId => {
-  const socket = socketIOClient('localhost:3000');
-  socket.emit('new-game', {playerId})
-}
--- new_client/src/service/words.js
+++
@@ -1,6 +0,0 @@
-import wretch from 'wretch';
-
-export const getGames = () =>
-  wretch('http://localhost:5000/api/games')
-    .get()
-    .json();
